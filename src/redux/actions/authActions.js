bb4490ade4d78f65e5f3c0c14c6027f9e484969d
leonardolombardi07 <leonardolombardi07@hotmail.com>
2020-04-27 19:50:19 -0300

    User Token done

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,69 +1,69 @@
-import { SIGN_IN, SIGN_OUT, TRY_LOCAL_SIGN_IN, ADD_ERROR } from './types';
-import { HerokuApiPost } from '../../apis/HerokuApi';
-import { AsyncStorage } from 'react-native';
-import * as RootNavigation from '../../routes/navigationFunctions/RootNavigation';
+import { SIGN_IN, SIGN_OUT, TRY_LOCAL_SIGN_IN, ADD_ERROR } from "./types";
+import { HerokuApiPost } from "../../apis/HerokuApi";
+import { AsyncStorage } from "react-native";
+import * as RootNavigation from "../../routes/navigationFunctions/RootNavigation";
 
 export const tryLocalSignIn = () => async (dispatch) => {
-    const refresh_token = await AsyncStorage.getItem('refresh_token');
-    if (refresh_token) {
-        const endPoint = `https://podio.com/oauth/token?grant_type=refresh_token&client_id=appfluxo&client_secret=snwczBivL56WeU2tHeA4oSPoxuOo0BL9VnmmWTOuESZ3x1m0VrDbPrj7bfBTzQEL&refresh_token=${refresh_token}`
-        
-        try {
-           const response = await HerokuApiPost.post(endPoint);
-           await AsyncStorage.setItem('access_token', response.data.access_token);
-           await AsyncStorage.setItem('refresh_token', response.data.refresh_token);
-        //    console.log(response.data.access_token)
-           dispatch({ 
-               type: TRY_LOCAL_SIGN_IN,
-               payload: {
-                   loadingAccessToken: false,
-                   access_token: response.data.access_token,
-                   refresh_token: response.data.refresh_token
-               }
-             })
-        } catch (error) {
-            alert(error.message)
-        };
+  const refresh_token = await AsyncStorage.getItem("refresh_token");
+  if (refresh_token) {
+    const endPoint = `https://podio.com/oauth/token?grant_type=refresh_token&client_id=appfluxo&client_secret=snwczBivL56WeU2tHeA4oSPoxuOo0BL9VnmmWTOuESZ3x1m0VrDbPrj7bfBTzQEL&refresh_token=${refresh_token}`;
 
-    } else {
-        // console.log("tryLocalSignIN em authActions chamada: não há refresh_token")
-        dispatch({ 
-            type: TRY_LOCAL_SIGN_IN,
-            payload: {
-                loadingAccessToken: false,
-                access_token: null,
-                refresh_token: null
-            }
-          })
+    try {
+      const response = await HerokuApiPost.post(endPoint);
+      await AsyncStorage.setItem("access_token", response.data.access_token);
+      await AsyncStorage.setItem("refresh_token", response.data.refresh_token);
+      await AsyncStorage.setItem("user_id", String(response.data.ref.id));
+
+      dispatch({
+        type: TRY_LOCAL_SIGN_IN,
+        payload: {
+          loadingAccessToken: false,
+          access_token: response.data.access_token,
+          refresh_token: response.data.refresh_token,
+        },
+      });
+    } catch (error) {
+      alert(error.message);
     }
-}
+  } else {
+    dispatch({
+      type: TRY_LOCAL_SIGN_IN,
+      payload: {
+        loadingAccessToken: false,
+        access_token: null,
+        refresh_token: null,
+      },
+    });
+  }
+};
 
 export const signIn = ({ email, password }) => async (dispatch) => {
-    try {
-        const json = JSON.stringify({ email: email, password: password });
-        const response = await HerokuApiPost.post ('/auth/', json);
-        await AsyncStorage.setItem ("access_token", response.data.access_token);
-        await AsyncStorage.setItem ("refresh_token", response.data.refresh_token);
-        dispatch ({ 
-            type: SIGN_IN, 
-            payload: { 
-            access_token: response.data.access_token, 
-            refresh_token: response.data.refresh_token
-         }
-        });
-        RootNavigation.navigate('Home')
+  try {
+    const json = JSON.stringify({ email: email, password: password });
+    const response = await HerokuApiPost.post("/auth/", json);
+    await AsyncStorage.setItem("access_token", response.data.access_token);
+    await AsyncStorage.setItem("refresh_token", response.data.refresh_token);
+    await AsyncStorage.setItem("user_id", String(response.data.ref.id));
 
-    } catch (error) {
-        dispatch({
-            type: ADD_ERROR,
-            payload: "Infelizmente não foi possível efetuar o Login"
-        });
-    };
-
-}
+    dispatch({
+      type: SIGN_IN,
+      payload: {
+        access_token: response.data.access_token,
+        refresh_token: response.data.refresh_token,
+      },
+    });
+    RootNavigation.navigate("Home");
+  } catch (error) {
+    dispatch({
+      type: ADD_ERROR,
+      payload: "Infelizmente não foi possível efetuar o Login",
+    });
+  }
+};
 
 export const signOut = () => async (dispatch) => {
-    await AsyncStorage.removeItem("access_token");
-    await AsyncStorage.removeItem("refresh_token");
-    dispatch({ type: SIGN_OUT })
+  await AsyncStorage.removeItem("access_token");
+  await AsyncStorage.removeItem("refresh_token");
+  await AsyncStorage.removeItem("user_id");
+  dispatch({ type: SIGN_OUT });
 };
--- src/redux/actions/plantaoActions.js
+++ src/redux/actions/plantaoActions.js
@@ -1,135 +1,134 @@
-import { CHANGE_PLANTAO_STATUS } from './types';
-import { HerokuApiGetAuth, HerokuApiPostAuth } from '../../apis/HerokuApi';
-import { AsyncStorage } from 'react-native';
+import { CHANGE_PLANTAO_STATUS } from "./types";
+import { HerokuApiGetAuth, HerokuApiPostAuth } from "../../apis/HerokuApi";
+import { AsyncStorage } from "react-native";
 import {
-    wasOpenedLessThen30MinutesAgo,
-    wasOpenedLessThen90MinutesAgo,
-    convertHourToPlantaoId
-} from '../../constants/functions/plantaoFunctions';
-import * as RootNavigation from '../../routes/navigationFunctions/RootNavigation';
-
+  wasOpenedLessThen30MinutesAgo,
+  wasOpenedLessThen90MinutesAgo,
+  convertHourToPlantaoId,
+} from "../../constants/functions/plantaoFunctions";
+import * as RootNavigation from "../../routes/navigationFunctions/RootNavigation";
 
 export const checkPlantaoStatus = () => async (dispatch) => {
-    const currentTime = new Date();
-    const token = await AsyncStorage.getItem("plantao_id");
-    
-    let statusPlantao = null;
-    let canChangePlantaoStatus = null;
+  const currentTime = new Date();
+  const token = await AsyncStorage.getItem("plantao_id");
 
-    
-    if (currentTime.getMinutes() > 20 && currentTime.getMinutes() < 50) {
-        if (!token) {
-            statusPlantao = 'Fechado';
+  let statusPlantao = null;
+  let canChangePlantaoStatus = false;
+
+  if (currentTime.getMinutes() > 50 || currentTime.getMinutes() < 20) {
+    if (!token) {
+      statusPlantao = "Fechado";
+      canChangePlantaoStatus = true;
+    } else if (token) {
+      statusPlantao = "Aberto";
+      try {
+        // Checar há quanto tempo foi marcado o último plantão
+        const response = await HerokuApiGetAuth.get("/plantao/");
+        const user_id = parseInt(await AsyncStorage.getItem("user_id"));
+
+        const horarios = [];
+        response.data.forEach((objeto) => {
+          if (objeto.usuario[0].user_id == user_id) {
+            if (objeto.status === "Aberto") {
+              horarios.push(
+                objeto.primeiro_plantao,
+                objeto.segundo_plantao,
+                objeto.horario_entrada
+              );
+            }
+          }
+        });
+
+        if (horarios.length === 0) {
+          //Nao foi encontrado, o plantão foi aberto há muito tempo
+          canChangePlantaoStatus = true;
+        } else if (horarios.length !== 0) {
+          if (wasOpenedLessThen30MinutesAgo(horarios[-1])) {
             canChangePlantaoStatus = true;
+          } else if (!wasOpenedLessThen30MinutesAgo(horarios[-1])) {
+            canChangePlantaoStatus = false;
+          }
         }
-        else if (token) {
-            statusPlantao = 'Aberto';
-            try {
-                // Checar há quanto tempo foi marcado o último plantão
-                const response = await HerokuApiGetAuth.get('/plantao/');
-                const user_id = await parseInt(AsyncStorage.getItem("user_id"));
-                const horarios = [];
-                response.data.forEach((objeto) => {
-                    if (objeto.usuario[0].user_id == user_id) {
-                        if (objeto.status === 'Aberto') {
-                            horarios.push(
-                                objeto.primeiro_plantao,
-                                objeto.segundo_plantao,
-                                objeto.horario_entrada
-                            );
-                        };
-                    };
-                });
-
-                if (horarios.length === 0) { //Nao foi encontrado, o plantão foi aberto há muito tempo
-                    canChangePlantaoStatus = true;
-                }
-                else if (horarios.length !== 0) {
-                    if (wasOpenedLessThen30MinutesAgo(horarios[-1])) {
-                        canChangePlantaoStatus = true;
-                    } else if (!wasOpenedLessThen30MinutesAgo(horarios[-1])) {
-                        canChangePlantaoStatus = false;
-                    };
-                };
-
-            } catch (error) {
-                alert(error.message)
-            };
-        };
+      } catch (error) {
+        alert(error.message);
+        canChangePlantaoStatus = true;
+      }
     }
-    else if (currentTime.getMinutes() < 20 || currentTime.getMinutes() > 50) {
-        canChangePlantaoStatus = false;
-        if (!token) {
-            statusPlantao = 'Fechado';
+    //currentTime.getMinutes() < 20 || currentTime.getMinutes() > 50
+    //currentTime.getMinutes() > 20 && currentTime.getMinutes() < 50
+  } else if (20 <= currentTime.getMinutes() <= 50) {
+    canChangePlantaoStatus = false;
+    if (!token) {
+      statusPlantao = "Fechado";
+    } else if (token) {
+      statusPlantao = "Aberto";
+    }
+  }
 
-        }
-        else if (token) {
-            statusPlantao = 'Aberto';
-        
-        };
-    };
-    
-    // console.log(statusPlantao, canChangePlantaoStatus)
+  // console.log(statusPlantao, canChangePlantaoStatus)
 
-    dispatch({ 
-        type: CHANGE_PLANTAO_STATUS,
-        payload: {
-            statusPlantao: statusPlantao,
-            canChangePlantaoStatus: canChangePlantaoStatus
-        }
-    });
-
+  dispatch({
+    type: CHANGE_PLANTAO_STATUS,
+    payload: {
+      statusPlantao: statusPlantao,
+      canChangePlantaoStatus: canChangePlantaoStatus,
+    },
+  });
 };
-
 
 export const fecharPlantao = (qrCodeURL) => async (dispatch) => {
-        // alert("Fechar Plantao Chamado")
-        let token = await AsyncStorage.getItem("plantao_id");
-        token = parseInt(token);
-        try {
-            if (qrCodeURL !== 'https://fluxoconsultoria.poli.ufrj.br/appfluxoeobicho') throw "Por gentileza, scannear o QR code localizado na sede da Fluxo"
-            if (!token) throw "Sem token";
-            const json = JSON.stringify({ fechar: 1 });
-            const response = await HerokuApiPostAuth.put(`/plantao/${token}/`, json);
-            await AsyncStorage.removeItem("plantao_id");
-            dispatch({ 
-                type: CHANGE_PLANTAO_STATUS, 
-                payload: {
-                    statusPlantao: 'Fechado',
-                    canChangePlantaoStatus: true //VERIFICAR ISSO: dependendo do horário é false
-                }
-            });
-            
-        } catch (error) {
-            alert(error)
-        } finally {      
-            RootNavigation.navigate('Utilidades');
-        };
+  // alert("Fechar Plantao Chamado")
+  let token = await AsyncStorage.getItem("plantao_id");
+  token = parseInt(token);
+  try {
+    if (qrCodeURL !== "https://fluxoconsultoria.poli.ufrj.br/appfluxoeobicho")
+      throw "Por gentileza, scannear o QR code localizado na sede da Fluxo";
+    if (!token) throw "Sem token";
+    const json = JSON.stringify({ fechar: 1 });
+    const response = await HerokuApiPostAuth.put(`/plantao/${token}/`, json);
+    await AsyncStorage.removeItem("plantao_id");
+    dispatch({
+      type: CHANGE_PLANTAO_STATUS,
+      payload: {
+        statusPlantao: "Fechado",
+        canChangePlantaoStatus: true, //VERIFICAR ISSO: dependendo do horário é false
+      },
+    });
+  } catch (error) {
+    alert(error);
+  } finally {
+    RootNavigation.navigate("Utilidades");
+  }
 };
-
 
 export const abrirPlantao = (qrCodeURL) => async (dispatch) => {
-        try {
-            if (qrCodeURL !== 'https://fluxoconsultoria.poli.ufrj.br/appfluxoeobicho') throw "Por gentileza, scannear o QR code localizado na sede da Fluxo"
-            let postID = convertHourToPlantaoId();
-            if (postID === "Não é possível abrir um plantão antes de 6AM ou depois de 17AM") throw "Não é possível abrir um plantão antes de 6AM ou depois de 17AM";
-            const json = JSON.stringify({ primeiro_plantao: postID, segundo_plantao: null }) // lidar com marcação de dois plantoes
-            const response = await HerokuApiPostAuth.post('/plantao/', json);
-            await AsyncStorage.setItem("plantao_id", String(response.data.item_id));
-            console.log(json)
+  try {
+    if (qrCodeURL !== "https://fluxoconsultoria.poli.ufrj.br/appfluxoeobicho")
+      throw "Por gentileza, scannear o QR code localizado na sede da Fluxo";
+    let postID = convertHourToPlantaoId();
+    if (
+      postID ===
+      "Não é possível abrir um plantão antes de 6AM ou depois de 17AM"
+    )
+      throw "Não é possível abrir um plantão antes de 6AM ou depois de 17AM";
+    const json = JSON.stringify({
+      primeiro_plantao: postID,
+      segundo_plantao: null,
+    }); // lidar com marcação de dois plantoes
+    const response = await HerokuApiPostAuth.post("/plantao/", json);
+    await AsyncStorage.setItem("plantao_id", String(response.data.item_id));
+    console.log(json);
 
-            dispatch({ 
-                type: CHANGE_PLANTAO_STATUS, 
-                payload: {
-                    statusPlantao: 'Aberto',
-                    canChangePlantaoStatus: false
-                }
-            });
-
-        } catch (error) {
-            alert("Erro na função abrirPlantao: " + error)
-        } finally {
-            RootNavigation.navigate('Utilidades');
-        };
+    dispatch({
+      type: CHANGE_PLANTAO_STATUS,
+      payload: {
+        statusPlantao: "Aberto",
+        canChangePlantaoStatus: false,
+      },
+    });
+  } catch (error) {
+    alert("Erro na função abrirPlantao: " + error);
+  } finally {
+    RootNavigation.navigate("Utilidades");
+  }
 };
-
